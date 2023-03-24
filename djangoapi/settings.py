8dc2f47c1c19c6abbde1348346c1cc305b742c17
Ashish Kumar <51400448+ashishsingh99@users.noreply.github.com>
2023-03-24 09:42:12 +0530

    Update settings.py

--- djangoapi/settings.py
+++ djangoapi/settings.py
@@ -177,7 +177,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_HOST_USER = 'singhashish1.sh@gmail.com'
-EMAIL_HOST_PASSWORD = 'rsjmralqdrffqqad'
+EMAIL_HOST_PASSWORD = 'ttavpkdyuirnceex'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
